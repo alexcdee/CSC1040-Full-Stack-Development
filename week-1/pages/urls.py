236046614e51abdb8d709c8f7bbe66e58cddbb33
Alexander Abramov <alexander.abramov2@dcu.ie>
2026-02-05 11:04:12 +0000

    Move project from week-1 to notes

--- week-1/pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('users/<int:id>/', views.user_profile),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-]
